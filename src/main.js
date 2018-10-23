c088a79c36f188bb3a46da42e0f581d8f603ed8c
Gabriel Dunn <gabe.dunn@shaw.ca>
2018-10-22 21:28:09 -0600

    remove vuex

--- src/main.js
+++ src/main.js
@@ -3,13 +3,11 @@ import VueForm from 'vue-form'
 
 import App from './App.vue'
 import router from './router'
-import store from './store'
 
 Vue.config.productionTip = false
 Vue.use(VueForm)
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
