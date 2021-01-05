769df94297ab00dc5db514ca9f56b931a8b52efc
Gabe Dunn <gabed@hey.com>
2021-01-05 12:10:47 -0700

    remove server

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
-import VueForm from 'vue-form'
 import VueImg from 'v-img'
 
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
-Vue.use(VueForm)
 Vue.use(VueImg, {
   altAsTitle: true,
   sourceButton: true
